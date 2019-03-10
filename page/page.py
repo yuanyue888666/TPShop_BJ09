41ffac5934af404ff32ee5824b1c02e8b3bb4a6d
John Doe <johndoe@example.com>
2019-03-10 20:09:21 +0800

    first commit

---
+++ page/page.py
@@ -0,0 +1,21 @@
+from page.home_page import HomePage
+from page.login_and_sign_up_page import LoginAndSignUpPage
+from page.mine_page import MinePage
+
+
+class Page:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @property
+    def home(self):
+        return HomePage(self.driver)
+
+    @property
+    def mine(self):
+        return MinePage(self.driver)
+
+    @property
+    def login_and_sign_up_page(self):
+        return LoginAndSignUpPage(self.driver)
---
+++ scripts/test_login.py
@@ -0,0 +1,26 @@
+import time
+import pytest
+
+from base.base_analyze import analyze_file
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestLogin:
+
+    def setup(self):
+        self.driver = init_driver()
+        self.page = Page(self.driver)
+
+    def teardown(self):
+        time.sleep(3)
+        self.driver.quit()
+
+    @pytest.mark.parametrize(("phone", "password", "expect"), analyze_file("login_data", "test_login"))
+    def test_login(self, phone, password, expect):
+        self.page.home.click_mine()
+        self.page.mine.click_login_and_sign_up()
+        self.page.login_and_sign_up_page.input_phone(phone)
+        self.page.login_and_sign_up_page.input_password(password)
+        self.page.login_and_sign_up_page.click_login()
+        assert self.page.login_and_sign_up_page.is_login(expect)
